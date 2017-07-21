69a9a1b2300af5bb9af697da5a6236841533c2cb
wmt-php1 <php1@webmobtech.com>
2017-07-21 20:34:55 +0530

    first commit

---
+++ js/datetime.js
@@ -0,0 +1,7 @@
+/**
+ * Created by shraddha on 21/7/17.
+ */
+
+exports.myDateTime = function () {
+    return Date();
+};
---
+++ js/lib.js
@@ -0,0 +1,12 @@
+/**
+ * Created by shraddha on 21/7/17.
+ */
+
+export function square(a) {
+    return a*a;
+}
+
+export function add(x,y)
+{
+    return x-y;
+}
